abbeb58ba6d229819e985b431c58b9448b7caf47
chengwei.vic <chengwei.vic@bytedance.com>
2021-11-24 17:25:48 +0800

    Initial commit

---
+++ network.py
@@ -0,0 +1,22 @@
+from node import Node
+
+
+class Network:
+    def __init__(self):
+        self.nodes = []
+        self.now_pt = None  # 当前节点指针
+
+    def remove_node(self, father_node):
+        pass
+
+
+class SimplexNetwork(Network):
+
+    def __init__(self):
+        super().__init__()
+
+
+class DuplexNetwork(Network):
+
+    def __init__(self):
+        super().__init__()
---
+++ node.py
@@ -0,0 +1,5 @@
+class Node:
+    def __init__(self):
+        self.next_node = None
+        self.uniqueID = None
+        self.is_disabled = False
